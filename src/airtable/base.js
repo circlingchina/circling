d48735cb9eccbb74296275994d77e2e1e4ca0740
e2 <esquaredly@gmail.com>
2020-04-27 11:15:54 -0700

    Reformat

--- src/airtable/base.js
+++ src/airtable/base.js
@@ -3,12 +3,12 @@ const Airtable = require('airtable');
 // test base url: https://airtable.com/tblFcEbcgAlnEsx9U/viwj26rVGRkbsl6Fe?blocks=hide
 // production base url: https://airtable.com/tblQjTFK3KpZxu61L/viw2dojPTqjU1UwGH?blocks=hide
 
-const baseId = process.env.NODE_ENV === 'production' ? "app53ecZ2UL9M6JOw" : "appMlvxAhkLM3TbcL"
+const baseId = process.env.NODE_ENV === 'production' ? "app53ecZ2UL9M6JOw" : "appMlvxAhkLM3TbcL";
 const base = new Airtable({ 
-    apiKey: process.env.AIRBASE_API_KEY 
+  apiKey: process.env.AIRBASE_API_KEY 
 }).base(baseId);
 
 module.exports = {
-    Users:  base('Users'),
-    OpenEvents: base('OpenEvents')
+  Users:  base('Users'),
+  OpenEvents: base('OpenEvents')
 };
