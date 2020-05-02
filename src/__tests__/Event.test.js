f4a2a2b82c13b0270a0f69427fd52a2546d21dab
e2 <esquaredly@gmail.com>
2020-05-02 12:44:23 -0700

    A Event wrapper class with all the helper methods needed to refactor the view

--- src/__tests__/Event.test.js
+++ src/__tests__/Event.test.js
@@ -1,6 +1,108 @@
 import Event from "../models/Event";
 
-test('event isFull works', () => {
-  const event1 = new Event({});
+test('constructor validates json for required fields', () => {
+  expect(() => new Event({})).toThrowError(new Error("event must have id"));
+  expect(() => new Event({id: 5})).toThrowError(new Error("event must have fields"));
+});
+
+test('event isFull returns false when there are less users than MaxAttendee', () => {
+  const event1 = new Event({
+    id: "abc",
+    fields: {
+      MaxAttendees: 7,
+      Users: [
+        "user_id_1"
+      ],
+    }
+  });
   expect(event1.isFull()).toBe(false);
+});
+
+test('event isFull returns true when there are less users than MaxAttendee', () => {
+  const event1 = new Event({
+    id: "abc",
+    fields: {
+      MaxAttendees: 1,
+      Users: [
+        "user_id_1"
+      ],
+    }
+  });
+  expect(event1.isFull()).toBe(true);
+});
+
+test('event isFull returns true when MaxAttendee is 0', () => {
+  const event1 = new Event({
+    id: "abc",
+    fields: {
+      MaxAttendees: 0,
+      Users: [],
+    }
+  });
+  expect(event1.isFull()).toBe(true);
+});
+
+test('event.containsUser() returns true if and only if user is in event', () => {
+  const event1 = new Event({
+    id: "abc",
+    fields: {
+      MaxAttendees: 0,
+      Users: ["bob", "carl"],
+    }
+  });
+  expect(event1.containsUser("alice")).toBe(false);
+  expect(event1.containsUser("bob")).toBe(true);
+  expect(event1.containsUser("carl")).toBe(true);
+});
+
+const minutesFromNow = (minutes) => {
+  return new Date(new Date().getTime() + (minutes * 60 * 1000));
+};
+
+test('event.startingStatus() returns NOT_STARTED for events 2 hours into future', () => {
+  let isoStr = minutesFromNow(300).toISOString();
+  let event = new Event({
+    id: "abc",
+    fields: { Time: isoStr }
+  });
+
+  expect(event.startingStatus()).toBe(Event.Status.NOT_STARTED);
+});
+
+
+test('event.startingStatus() returns STARTING_SOON for events within 2 hours', () => {
+  let isoStr = minutesFromNow(60).toISOString();
+  let event = new Event({
+    id: "abc",
+    fields: { Time: isoStr }
+  });
+
+  expect(event.startingStatus()).toBe(Event.Status.STARTING_SOON);
+});
+
+test('event.startingStatus() returns ONGOING for events between 15 min to 2 hours running', () => {
+  let isoStr = minutesFromNow(10).toISOString();
+  let event = new Event({
+    id: "abc",
+    fields: { Time: isoStr }
+  });
+  expect(event.startingStatus()).toBe(Event.Status.ONGOING);
+});
+
+test('event.startingStatus() returns ONGOING for events between 15 min to 2 hours running', () => {
+  let isoStr = minutesFromNow(-60).toISOString();
+  let event = new Event({
+    id: "abc",
+    fields: { Time: isoStr }
+  });
+  expect(event.startingStatus()).toBe(Event.Status.ONGOING);
+});
+
+test('event.startingStatus() returns PAST for events 2 hours ago or longer', () => {
+  let isoStr = minutesFromNow(-180).toISOString();
+  let event = new Event({
+    id: "abc",
+    fields: { Time: isoStr }
+  });
+  expect(event.startingStatus()).toBe(Event.Status.FINISHED);
 });
--- src/models/Event.js
+++ src/models/Event.js
@@ -1,9 +1,67 @@
+/**
+ * A wrapper class around the event json object returned from the airtabel API.
+ * Example JSON:
+ *  {
+      "id": "recKj8lRP6ZSOBfvl",
+      "fields": {
+        "Name": "Circling 周二会员活动",
+        "Time": "2020-05-28T13:00:00.000Z",
+        "MaxAttendees": 7,
+        "Category": "每日Circling",
+        "Host": "Mint (DEV)",
+        "Users": [
+          "recIdsEqRLNV52zPC"
+        ],
+        "EventLink": "https://zhumu.me/j/1516471967"
+      }
+    }
+ */
+
 export default class Event {
   constructor(rawJson) {
+    if(!('id' in rawJson)) {
+      throw new Error("event must have id");
+    }
+    if(!('fields' in rawJson)) {
+      throw new Error("event must have fields");
+    }
     this.rawJson = rawJson;
   }
 
   isFull() {
-    return false;
+    const isFull = this.getUsers().length >= this.rawJson.fields.MaxAttendees;
+    return isFull;
+  }
+
+  //the field may not exist, return empty array instead of null
+  getUsers() {
+    return this.rawJson.fields.Users || [];
+  }
+  
+  containsUser(userId) {
+    return this.getUsers().includes(userId);
+  }
+
+  startingStatus() {
+    const msDiff = new Date(this.rawJson.fields.Time) - new Date();
+    const minutesUntil = msDiff / 1000 / 60;
+
+    if(minutesUntil > 2 * 60) {
+      return Event.Status.NOT_STARTED;
+    }
+    if(minutesUntil > 15){
+      return Event.Status.STARTING_SOON;
+    }
+    if(minutesUntil > -2 * 60) {
+      return Event.Status.ONGOING;
+    }
+    return Event.Status.FINISHED;
   }
 }
+
+Event.Status =  {
+  NOT_STARTED: "not_started",
+  STARTING_SOON: "starting_soon",
+  ONGOING: "ongoing",
+  FINISHED: "finished"
+};
