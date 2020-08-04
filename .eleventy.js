295be2f607541763674ca4a63141157b350b9d9e
e2 <esquaredly@gmail.com>
2020-08-04 15:57:32 -0700

    adding a redirect rule for netlify

--- .eleventy.js
+++ .eleventy.js
@@ -1,24 +1,27 @@
-const moment = require('moment');
+const moment = require("moment");
 
-moment.locale('zh-cn');
+moment.locale("zh-cn");
 
-module.exports = function(eleventyConfig) {
-  eleventyConfig.addPassthroughCopy('css')
-  eleventyConfig.addPassthroughCopy('js')
-  eleventyConfig.addPassthroughCopy('images')
-  eleventyConfig.addPassthroughCopy('admin')
-  eleventyConfig.addPassthroughCopy('_email_templates')
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addPassthroughCopy("css");
+  eleventyConfig.addPassthroughCopy("js");
+  eleventyConfig.addPassthroughCopy("images");
+  eleventyConfig.addPassthroughCopy("admin");
+  eleventyConfig.addPassthroughCopy("_email_templates");
+  eleventyConfig.addPassthroughCopy("_redirects");
 
   //add date filter
-  eleventyConfig.addFilter('dateIso', date => {
+  eleventyConfig.addFilter("dateIso", (date) => {
     return moment(date).toISOString();
   });
 
-  eleventyConfig.addFilter('dateReadable', date => {
-    return moment(date).format("YYYY年M月D日（ddd）H点m分").replace(/点0分$/, '点');
+  eleventyConfig.addFilter("dateReadable", (date) => {
+    return moment(date)
+      .format("YYYY年M月D日（ddd）H点m分")
+      .replace(/点0分$/, "点");
   });
 
   return {
-    passthroughFileCopy: true
-  }
-}
+    passthroughFileCopy: true,
+  };
+};
