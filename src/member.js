3b84ca3aa94e9cbc2f2eb2ce63e5ffb8ffc0b01b
e2 <esquaredly@gmail.com>
2020-08-04 10:25:01 -0700

    [WIP] added a calendar widget with fake events

--- src/member.js
+++ src/member.js
@@ -3,9 +3,37 @@ import ReactDOM from "react-dom";
 import EventsTable from "./components/EventsTable";
 import UpcomingEvent from "./components/UpcomingEvent";
 import api from "./circling-api";
+import { Calendar, momentLocalizer } from "react-big-calendar";
+import moment from "moment";
+
+function convertToCalendarEvents(events) {
+  //right now we return some fake events, but what we need to do is to covert events into the format the calendar needs
+
+  const calEvents = events.map((event) => {
+    return {
+      start: moment().toDate(),
+      end: moment().add(1, "days").toDate(),
+      title: "Fake Event 1",
+    };
+  });
+  return calEvents;
+}
+
+const MyCalendar = ({ events }) => (
+  <div>
+    <Calendar
+      localizer={momentLocalizer(moment)}
+      defaultDate={new Date()}
+      defaultView="month"
+      events={events}
+      startAccessor="start"
+      endAccessor="end"
+      style={{ height: 500 }}
+    />
+  </div>
+);
 
 function EventRegion() {
-
   const [events, setEvents] = useState([]);
   const [user, setUser] = useState({}); //empty object to be back-compatible (i.e can always call user.id, even if id is null)
 
@@ -22,22 +50,22 @@ function EventRegion() {
   }, []);
 
   const updateEvents = (changedEvent) => {
-    const newEvents = events.map((event)=> {
+    const newEvents = events.map((event) => {
       return event.id == changedEvent.id ? changedEvent : event;
     });
     setEvents(newEvents);
   };
 
-  window.netlifyIdentity.on('login', async (netiflyUser) => {
+  window.netlifyIdentity.on("login", async (netiflyUser) => {
     const result = await api.findUserByEmail(netiflyUser.email);
-    if(result.user) {
+    if (result.user) {
       setUser(result.user);
     } else {
       throw Error(`${netiflyUser.email} not found in user db!`);
     }
   });
 
-  window.netlifyIdentity.on('logout', () => {
+  window.netlifyIdentity.on("logout", () => {
     setUser({});
   });
 
@@ -47,7 +75,7 @@ function EventRegion() {
 
   return (
     <>
-      <div id='modal-root'></div>
+      <div id="modal-root"></div>
       <div className="section">
         <div className="container w-container">
           <UpcomingEvent events={events} userId={user.id} />
@@ -58,10 +86,16 @@ function EventRegion() {
       </div>
       <div className="section transparent">
         <div className="container w-container">
-          <div className="sub-text red">
-            本周会员Circling
+          <div className="sub-text red">本周会员Circling</div>
+          <div className="CalendarApp">
+            <MyCalendar events={convertToCalendarEvents(events)} />
           </div>
-          <EventsTable events={events} user={user} onEventChanged={updateEvents} onUserChanged={updateUser} />
+          <EventsTable
+            events={events}
+            user={user}
+            onEventChanged={updateEvents}
+            onUserChanged={updateUser}
+          />
         </div>
         <div className="page-divider">
           <div className="page-divider-white down" />
