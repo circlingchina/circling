e5b31eea707b5642ee029d99237c2c2c1a414246
Yiliang Tang <ydatylmonv@gmail.com>
2020-08-15 21:56:06 +0200

    schema change

---
+++ server/db/migrations/20200815_extend_event_category_and_add_event_credit.sql
@@ -0,0 +1,14 @@
+DO $$
+DECLARE new_version DATE;
+BEGIN
+  new_version := '2020-08-15' ;
+  UPDATE db_version SET db_version = new_version;
+END $$;
+
+ALTER TABLE users ADD COLUMN event_credit INTEGER NOT NULL DEFAULT 0;
+
+
+ALTER TYPE event_category ADD VALUE 'Circling周边游戏';
+ALTER TYPE event_category ADD VALUE '社群自发活动';
+ALTER TYPE event_category RENAME VALUE  '线上Circling' TO 'Circling';
+
