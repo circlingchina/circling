c5ab949c0c149dffd22a4ff0f7ab726588e691d7
Yiliang Tang <ydatylmonv@gmail.com>
2020-08-16 13:54:20 +0200

    schema change

--- server/db/migrations/20200815_extend_event_category_and_add_event_credit.sql
+++ server/db/migrations/20200815_extend_event_category_and_add_event_credit.sql
@@ -9,6 +9,9 @@ ALTER TABLE users ADD COLUMN event_credit INTEGER NOT NULL DEFAULT 0;
 
 
 ALTER TYPE event_category ADD VALUE 'Circling周边游戏';
-ALTER TYPE event_category ADD VALUE '社群自发活动';
+
 ALTER TYPE event_category RENAME VALUE  '线上Circling' TO 'Circling';
+ALTER TYPE event_category RENAME VALUE  '亚隆团体' TO '社群自发活动';
+
+ALTER TYPE event_category RENAME VALUE  '付费活动' TO '付费活动 - deprecated';
 
