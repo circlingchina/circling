6efc0dc35207811cf4927dc2070e2e0325a97780
e2 <esquaredly@gmail.com>
2020-08-25 17:20:22 -0700

    Payment for half year now uses different link
    fix #67

--- src/payment.js
+++ src/payment.js
@@ -1,24 +1,26 @@
-
 import React from "react";
 import ReactDOM from "react-dom";
 import PaymentButton from "./components/PayButton";
 
 const paymentSingleEvent = document.getElementById("payment-single-event");
 if (paymentSingleEvent) {
-  ReactDOM.render(<PaymentButton chargeType='SINGLE_EVENT'/>, paymentSingleEvent);
+  ReactDOM.render(
+    <PaymentButton chargeType="SINGLE_EVENT" />,
+    paymentSingleEvent
+  );
 }
 
 const paymentMonthly = document.getElementById("payment-monthly");
 if (paymentMonthly) {
-  ReactDOM.render(<PaymentButton chargeType='MONTHLY'/>, paymentMonthly);
+  ReactDOM.render(<PaymentButton chargeType="MONTHLY" />, paymentMonthly);
 }
 
-const paymentHalfYear = document.getElementById("payment-half-year");
-if (paymentHalfYear) {
-  ReactDOM.render(<PaymentButton chargeType='HALF_YEAR'/>, paymentHalfYear);
-}
+// const paymentHalfYear = document.getElementById("payment-half-year");
+// if (paymentHalfYear) {
+//   ReactDOM.render(<PaymentButton chargeType='HALF_YEAR'/>, paymentHalfYear);
+// }
 
 const paymentVip = document.getElementById("payment-vip");
-if (paymentHalfYear) {
-  ReactDOM.render(<PaymentButton chargeType='VIP'/>, paymentVip);
+if (paymentMonthly) {
+  ReactDOM.render(<PaymentButton chargeType="VIP" />, paymentVip);
 }
