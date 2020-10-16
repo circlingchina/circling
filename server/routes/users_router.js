6cc7612ef5cb9d72e7509799f09aa20b53313e56
Yiliang Tang <ydatylmonv@gmail.com>
2020-10-16 21:11:58 +0200

    Deprecate direct user routers and signup hook

--- server/routes/users_router.js
+++ server/routes/users_router.js
@@ -1,21 +1,4 @@
-const debug = require("debug")("server");
 const db = require("../db");
-
-const update = async(req, res) => {
-
-  const id = req.params.userId;
-  const userParams = req.body; //{email: 'some@email.com'}
-  const users = await db("users").returning('*').update(userParams).where({id});
-
-  const user = users.length > 0 ? users[0] : null;
-
-  res
-    .status(200)
-    .type('json')
-    .send(JSON.stringify({
-      user
-    }));
-};
 
 const find = async(req, res) => {
   const query = req.query;
@@ -30,33 +13,7 @@ const find = async(req, res) => {
     }));
 };
 
-const create = async(req, res) => {
-  const userParams = req.body;
-  // upsert - on conflict update
-  const query = db.raw(
-    `? ON CONFLICT (email)
-            DO UPDATE SET
-            name = EXCLUDED.name
-          RETURNING id;`,
-    [db("users").insert(userParams)],
-  );
-  const result = await query;
-  let id = null;
-  if(result.rowCount > 0) {
-    id = result.rows[0].id;
-  }
-  debug({id});
-  res
-    .status(200)
-    .type('json')
-    .send(JSON.stringify({
-      id
-    }));
-};
 
 module.exports = (app) => {
   app.get('/users/find', find);
-  app.post('/users', create);
-  app.put('/users/:userId', update);
-
 };
