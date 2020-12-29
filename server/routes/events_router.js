67fff614836a5a91b1e480ced5ff06ab71a2e5f4
Yiliang Tang <ydatylmonv@gmail.com>
2020-12-29 20:32:23 +0100

    Hotfix: join api auth issue

--- server/routes/events_router.js
+++ server/routes/events_router.js
@@ -63,7 +63,7 @@ const join = async (req, res) => {
 
   const event = await Event.find(eventId, {includeAttendees: true});
 
-  if (Event.isInJoinableTimeFrame(event)) {
+  if (!Event.isInJoinableTimeFrame(event)) {
     res
       .status(400)
       .type('json')
